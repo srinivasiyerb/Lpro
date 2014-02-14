10e004e5c8dd703df34f53638225b2c01e1cbded
srinivas <srinivasiyerb@gmail.com>
2014-02-14 17:31:08 +0530

    Update Keywords.java

--- LitPro/src/test/java/SLZ_LP/LitPro/Keywords.java
+++ LitPro/src/test/java/SLZ_LP/LitPro/Keywords.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 // testng
@@ -27,7 +27,7 @@ public class Keywords extends AppTest{
 		APPICATION_LOGS.debug("Executing Navigate");
 		if(wbdv == null){
 			if(CONFIG.getProperty("testBrowser").equals("Firefox")){
-				wbdv = new FirefoxDriver();
+				wbdv = new HtmlUnitDriver();
 //				wbdv = new ChromeDriver();
 				driver = new EventFiringWebDriver(wbdv);
 			//	driver = new HtmlUnitDriver();
